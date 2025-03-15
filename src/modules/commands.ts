6d7e888e8723e715e48e999ec6da3e2f8c34c2ef
Anze Mavric <mavric.insane@gmail.com>
2025-03-15 16:55:43 +0100

    fixed command hooks

--- src/modules/commands.ts
+++ src/modules/commands.ts
@@ -1,6 +1,4 @@
 export { handleStreamCommand } from './commands/stream';
 export { handleStopCommand } from './commands/stop';
-export { handleJoinCommand } from './commands/join';
-export { handleLeaveCommand } from './commands/leave';
 export { handleListCommand } from './commands/list';
 export { handleRefreshCommand } from './commands/refresh';
